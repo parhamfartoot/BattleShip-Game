78b8b0d0b655a4d5a488868c6690aaf6ac43e026
Sin Hwan Lee <jamessh.lee@mail.utoronto.ca>
2019-02-17 15:32:17 -0500

    Added StartMenu and changes to View

--- src/Battleship/View.java
+++ src/Battleship/View.java
@@ -11,9 +11,9 @@ public class View implements Observer {
     private GameModel model;
     private Stage stage;
     private BorderPane root = new BorderPane();
+    private Menu gamemenu = new Menu(0);
 
     protected View(Stage stage) {
-
         this.model = GameModel.getInstance();
         this.stage = stage;
         setObservable();
@@ -21,14 +21,11 @@ public class View implements Observer {
     }
 
     private void initUI(Stage stage) {
-
-        //Instantiate the view with Transition
-        Transition trans = new Transition();
-        trans.button.setText("Start !");
-        root.setCenter(trans);
+        //Instantiate the view with Start Menu
+        root.setCenter(gamemenu);
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.setTitle("Player " + model.Turn());
+        stage.setTitle("Welcome to BattleShips!");
         stage.show();
     }
 
@@ -41,15 +38,19 @@ public class View implements Observer {
     public void update(Observable o, Object arg) {
         // Clears the Pane
         root.getChildren().clear();
-
-        // If the game is in transition phase Displays the Transition, otherwise the according board
-        if (model.State()){
-            this.root.setCenter(model.GetPlayer());
-    }
-        else{
-            root.setCenter(new Transition());
+        //If game options were selected during Start Menu
+        if (model.getSelection() > 1){
+	        // If the game is in transition phase Displays the Transition, otherwise the according board
+	        if (model.State()){
+	            this.root.setCenter(model.GetPlayer());
+	        } else{
+	            root.setCenter(new Transition());
+	        }
+	        // Sets the title to the current player
+	        stage.setTitle("Player " + model.Turn());
+	    // Start Menu Selection
+        } else {
+        	this.root.setCenter(new Menu(model.getSelection()));
         }
-        // Sets the title to the current player
-        stage.setTitle("Player " + model.Turn());
     }
 }
